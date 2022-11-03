746bb97fcfd5faa49b79717e6adc84289c789771
Ruoyuan Du <57498371+ruoyuanDu@users.noreply.github.com>
2022-11-02 21:52:03 -0400

    request -> requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,5 +20,5 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show )
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 stremlit.text(fruityvice_response.json)
