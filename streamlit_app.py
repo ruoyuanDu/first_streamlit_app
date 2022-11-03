6ed64cbc017d27052a64c984b98b219d7aca627d
Ruoyuan Du <57498371+ruoyuanDu@users.noreply.github.com>
2022-11-02 22:24:10 -0400

    convert json to dataframe and make a new table

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,3 +22,9 @@ streamlit.dataframe(fruits_to_show )
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# flatten the json format to tabluar form dataframe
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# make a table on streamlit
+streamlit.dataframe(fruityvice_normalized)
+
