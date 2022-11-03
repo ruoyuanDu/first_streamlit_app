610d780ca6bc424d535ed8fba6073208e8aa1ed0
Ruoyuan Du <57498371+ruoyuanDu@users.noreply.github.com>
2022-11-03 08:38:51 -0400

    add customize fruit_choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,7 +20,11 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show )
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+# Add a Text Entry Box and Send the Input to Fruityvice as Part of the API Call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 streamlit.text(fruityvice_response.json())
 
 # flatten the json format to tabluar form dataframe
